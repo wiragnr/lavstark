bb2f107a75192dd188cc089106ca0d8728b1ec7f
wiragnr <161958091+wiragnr@users.noreply.github.com>
2024-04-25 17:24:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"starknet_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.starknet.lava.build/lava-referer-48f5c988-8455-4951-b132-d47436f87539/)
 
 
 echo "$curl_output" >> Update.tsx
